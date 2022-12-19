762191b3041c9a23e45692bd6b8c7bcf0f4bf814
DeshmukhMandar3 <mandardeshmukh.mud@gmail.com>
2022-12-19 11:32:51 +0530

    backend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
 
-const PORT = 8080
+const PORT = 8000
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`)
